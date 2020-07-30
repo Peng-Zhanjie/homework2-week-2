69a077f8734f45cb6f68628754ed9971b3f05167
Peng-Zhanjie <nowarzenx@gmail.com>
2020-07-31 01:04:58 +0800

    Discout_and_its_output

---
+++ capitalist_conrad.py
@@ -0,0 +1,29 @@
+import random
+MAX_INCREASE = 0.175  # 17.5%
+MAX_DECREASE = 0.05  # 5%
+MIN_PRICE = 1.0
+MAX_PRICE = 100.0
+INITIAL_PRICE = 10.0
+
+price = INITIAL_PRICE
+out_file = open("OUTPUT_FILE", 'w')
+print("Starting price: ${:,.2f}".format(price),file=out_file)
+Daytime=0;
+while price >= MIN_PRICE and price <= MAX_PRICE:
+    price_change = 0
+    # generate a random integer of 1 or 2
+    # if it's 1, the price increases, otherwise it decreases
+    if random.randint(1, 2) == 1:
+        # generate a random floating-point number
+        # between 0 and MAX_INCREASE
+        price_change = random.uniform(0, MAX_INCREASE)
+        Daytime+=1
+    else:
+        # generate a random floating-point number
+        # between negative MAX_DECREASE and 0
+        price_change = random.uniform(-MAX_DECREASE, 0)
+        Daytime+=1
+
+    price *= (1 + price_change)
+    print("On day {} price is:${:,.2f}".format(Daytime,price),file=out_file)
+out_file.close()
