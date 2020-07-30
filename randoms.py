b59ae1e92c574a1a5a903c47fb59e0a72211cb2c
Peng-Zhanjie <nowarzenx@gmail.com>
2020-07-30 16:25:06 +0800

    Randoms file

---
+++ randoms.py
@@ -0,0 +1,21 @@
+import random
+
+print(random.randint(5, 20))  # line 1
+print(random.randrange(3, 10, 2))  # line 2
+print(random.uniform(2.5, 5.5))  # line 3
+
+# What did you see on line 1?
+# What was the smallest number you could have seen, what was the largest?
+
+# a random int in 5 to 20,the smallest is 5, the biggest is 20.
+
+# What did you see on line 2?
+# What was the smallest number you could have seen, what was the largest?
+# Could line 2 have produced a 4?
+
+# a odd number in 3 to 10, the smallest is 3, the biggest is 9.
+
+# What did you see on line 3?
+# What was the smallest number you could have seen, what was the largest?
+
+# a random float from 2.5 to 5.5, the smallest is 2.5, the biggest is 5.5.
